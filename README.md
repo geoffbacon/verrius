f04554b49828181394f04fb9e1a753acad03ca7e
Geoff Bacon <bacon@berkeley.edu>
2020-01-06 00:50:32 -0800

    finished EDAP notebook

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Verrius
 
 Part-of-speech tagger and lemmatizer for Latin.
+
+## To do
+- POS tagging
+- External unlabelled data
+- External labelled data
+- Data augmentation methods
---
+++ src/filenames.py
@@ -0,0 +1,5 @@
+"""File name constants used across different modules."""
+
+RAW_EVALATIN_TRAINING_DATA_DIR = "data/evalatin/raw"
+PROCESSED_EVALATIN_POS_TRAIN_DATA = "data/evalatin/processed/pos.txt"
+ROOT = "/Volumes/GoogleDrive/My Drive/research/evalatin/verrius"
