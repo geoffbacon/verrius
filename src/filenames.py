3e659f14f22ce3f6d8565ba1d8cb4cf9c4aed27c
Geoff Bacon <bacon@berkeley.edu>
2020-01-06 21:40:17 -0800

    clarify root

--- src/filenames.py
+++ src/filenames.py
@@ -1,5 +1,9 @@
 """File name constants used across different modules."""
 
-ROOT = "/Volumes/GoogleDrive/My Drive/research/evalatin/verrius"
+remote = False
+if remote:
+    ROOT = "/home/bacon/verrius"
+else:
+    ROOT = "/Volumes/GoogleDrive/My Drive/research/evalatin/verrius"
 RAW_EVALATIN_TRAINING_DATA_DIR = "data/evalatin/raw"
 CONFIG_FILENAME = "src/pos.jsonnet"
